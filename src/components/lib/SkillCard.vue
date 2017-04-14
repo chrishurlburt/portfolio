774f0cf60713f8066f1df13f77b4818dea91bf94
Chris Hurlburt <churlburt132@g.rwu.edu>
2017-04-14 01:12:29 -0400

    scroll transitions

--- src/components/lib/SkillCard.vue
+++ src/components/lib/SkillCard.vue
@@ -1,14 +1,20 @@
 <template>
   <section class="skill-card" :class="{ flipped }">
-    <div class="skill-heading">
-      <div class="skill-heading--inner">
-        <h4 class="skill-title"><slot name="skill"></slot></h4>
-        <p class="skill-description"><slot name="description"></slot></p>
+    <div class="skill-card-marker"></div>
+    <div class="skill-card--inner">
+      <div class="skill-heading">
+          <div class="skill-title-wrap">
+            <div class="skill-title-block" :style="{ transform: (reveal) ? 'scaleX(0)' : 'scaleX(1)' }"></div>
+            <h4 class="skill-title"><slot name="skill"></slot></h4>
+          </div>
+          <span class="divider" :style="{ transform: (reveal) ? 'scaleX(1)' : 'scaleX(0)' }"></span>
       </div>
-    </div>
-    <div class="skill-slider">
-      <div class="skill-slider--inner">
-
+      <div class="skill-description">
+        <transition name="fade" @after-enter="finish">
+          <p class="skill-description" :key="reveal" :style="{ visibility: reveal || 'hidden' }">
+            <slot name="description"></slot>
+          </p>
+        </transition>
       </div>
     </div>
   </section>
@@ -16,11 +22,35 @@
 
 <script>
 export default {
+  data() {
+    return {
+      reveal: false,
+      finished: false,
+    }
+  },
+  watch: {
+    animate(val) {
+      if (val && !this.finished) this.reveal = true
+    },
+  },
+  methods: {
+    finish() {
+      this.finished = true
+      this.$emit('entered')
+    },
+  },
+  mounted() {
+    if (this.animate) this.reveal = true
+  },
   props: {
     flipped: {
       type: Boolean,
-      default: () => false
-    }
+      default: () => false,
+    },
+    animate: {
+      type: Boolean,
+      default: () => false,
+    },
   },
 }
 </script>
--- src/components/lib/Skills.vue
+++ src/components/lib/Skills.vue
@@ -6,31 +6,35 @@
       </transition>
     </div>
     <div class="skills--inner">
-      <Skill-card>
-        <template slot="skill">Backend</template>
-        <template slot="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</template>
-      </Skill-card>
-      <Skill-card :flipped="true">
-        <template slot="skill">Frontend</template>
-        <template slot="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</template>
-      </Skill-card>
-      <Skill-card>
-        <template slot="skill">iOS + Android</template>
-        <template slot="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</template>
-      </Skill-card>
-      <Skill-card :flipped="true">
-        <template slot="skill">Dev Ops</template>
-        <template slot="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</template>
-      </Skill-card>
-      <Skill-card>
-        <template slot="skill">Prototyping</template>
-        <template slot="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</template>
-      </Skill-card>
+      <div class="skill-line"></div>
+      <div class="skills-list">
+        <Skill-card :animate="shouldAnimate('backend')" key="backend">
+          <template slot="skill">Backend</template>
+          <template slot="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</template>
+        </Skill-card>
+        <Skill-card :flipped="true" :animate="shouldAnimate('frontend')" key="frontend">
+          <template slot="skill">Frontend</template>
+          <template slot="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</template>
+        </Skill-card>
+        <Skill-card :animate="shouldAnimate('ios')" key="ios">
+          <template slot="skill">iOS + Android</template>
+          <template slot="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</template>
+        </Skill-card>
+        <Skill-card :flipped="true" :animate="shouldAnimate('ops')" key="ops">
+          <template slot="skill">Dev Ops</template>
+          <template slot="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</template>
+        </Skill-card>
+        <Skill-card :animate="shouldAnimate('prototyping')" key="prototyping">
+          <template slot="skill">Prototyping</template>
+          <template slot="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</template>
+        </Skill-card>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
+import throttle from 'lodash.throttle'
 import SkillCard from './SkillCard'
 
 export default {
@@ -40,21 +44,45 @@ export default {
   data() {
     return {
       transitionSkillsHeader: false,
+      transitionSkillCards: [],
       finished: false,
     }
   },
   watch: {
-    // animate(val) {
-    //   if (!this.finished && val) this.transitionSkillsHeader = true
-    // },
-  },
-  methods: {
-    skillsTitleTransitioned() {
-
+    animate(val) {
+      if (!this.finished && val) this.transitionSkillsHeader = true
     },
   },
-  created() {
+  methods: {
+    shouldAnimate(key) {
+      return this.transitionSkillCards.length === 5 || this.transitionSkillCards.includes(key)
+    },
+    skillCardsReduce(map, vm) {
+      return map.set(vm, vm.$el.getBoundingClientRect().top)
+    },
+    checkSkillCard() {
+      const scrolled = window.scrollY
+      this.skillCardsLocations.forEach((distanceTop, vm) => {
+        const toTopOfViewport = distanceTop - scrolled
+        if (toTopOfViewport + 400 <= window.innerHeight && toTopOfViewport > 0) {
+          this.transitionSkillCards.push(vm.$vnode.key)
+          this.skillCardsLocations.delete(vm)
+        }
+      })
+    },
+  },
+  mounted() {
     if (this.animate) setTimeout(() => this.transitionSkillsHeader = true, 200)
+    setTimeout(() => {
+      window.scrollTo(0, 0)
+      this.skillCardsLocations = this.$children.reduce(this.skillCardsReduce, new Map())
+      window.onscroll = throttle(this.checkSkillCard, 100)
+    }, 50)
+    // FOUC leads to unreliable initial element locations -- can probably remove
+    // this in production.
+  },
+  beforeDestroy() {
+    window.onscroll = undefined
   },
   props: {
     animate: {
