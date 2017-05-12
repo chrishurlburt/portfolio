c26d516930693c3d0b913b6fe9fac1b86724a969
Chris Hurlburt <churlburt132@g.rwu.edu>
2017-05-11 23:20:47 -0400

    about page copy v2

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -2,10 +2,7 @@
   <section class="about-page">
     <h2 class="about-title">Nice to meet you.</h2>
     <p class="about-bio">
-      Hi, my name is Chris. I’m a product focused web developer, creative thinker and problem solver. <span class="bold">I help businesses extend their product to the web through tailored platforms vital to reaching a digitally-minded world.</span>
-    </p>
-    <p>
-      Working closely with clients, I analyze the specific needs of a project and devise innovative solutions through web technologies. I believe strongly in UX — the user’s needs must inform the composition of the end product to create a symbiotic relationship between form and function. With this in mind, I take on a limited number of projects to ensure precise attention to detail and development velocity.
+      Hi, my name is Chris. I’m a web developer who creates engaging, intelligent and beautiful digital platforms. I believe digital accessibility for businesses is the greatest variable of success in a technology-driven world. User experience is everything and convenience by simplicity is essential. Through websites, web applications, mobile apps, or web infrastructure, <span class="bold">let’s work together to elevate your product.</span>
     </p>
   </section>
 </template>
